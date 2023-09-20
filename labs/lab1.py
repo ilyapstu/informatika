f734f9ed7ab246d443979e300b5a18697df4fe8e
ilyapstu <144197003+ilyapstu@users.noreply.github.com>
2023-09-20 11:13:22 +0500

    Сделал 1 задание

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,4 +1,10 @@
-import math 
+import math as m
+x = int(input())
+y = int(input())
+z = int(input())
 
-def task1():
-  pass
+a = m.log10(abs(x-1)) + ((abs(x))**(1/4))/ (x**(1/4) + (abs(x)**(1/4) * m.log10(y)))
+
+b = (abs(x-1)**(1/3)) + (m.cos(z)/m.tan(x**2))
+
+print(a,b)
