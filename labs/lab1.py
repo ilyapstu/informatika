a996db81e923063e05d4f23364a21a825349221f
ilyapstu <144197003+ilyapstu@users.noreply.github.com>
2023-09-27 10:08:51 +0500

    Задание 2 сделано

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,3 +1,4 @@
+#Задание 1
 import math as m
 x = int(input())
 y = int(input())
@@ -8,3 +9,13 @@ a = m.log10(abs(x-1)) + ((abs(x))**(1/4))/ (x**(1/4) + (abs(x)**(1/4) * m.log10(
 b = (abs(x-1)**(1/3)) + (m.cos(z)/m.tan(x**2))
 
 print(a,b)
+
+#Задание 2
+def task2(x):
+    a = 1
+    b = 2
+    return (a * (x ** 3) + x) / (b * (x ** 2)) + ((x ** 2) ** (1 / 3))
+
+
+x = float(input(1))
+print(task2(x))
