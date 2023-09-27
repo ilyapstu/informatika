822af9f24821ae27baf6ed9bc862d5dc4474523b
ilyapstu <144197003+ilyapstu@users.noreply.github.com>
2023-09-27 20:15:21 +0500

    Исправить c1 и c2

--- labs/lab1.py
+++ labs/lab1.py
@@ -63,3 +63,17 @@ print(S_osn)
 
 
 #Задание 7
+a1 = int(input())
+a2 = int(input())
+b1 = int(input())
+b2 = int(input())
+c1 = int(input())
+c2 = int(input())
+d = a1 * b2 - a2 * b1
+x = ((c1 * b2) - (c2 * b1)) / d
+y = ((a1 * c2) - (a2 * c1)) / d
+c1 = (a1 * x) + (b1 * y) #Исправить c1 и c2
+c2 = (a2 * x) + (b2 * y)
+
+print((c1, c2))
+print(round(y, 4))
