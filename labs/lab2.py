d25a53e6e83f6891b9ba86181e8fab6de278c106
ilyapstu <144197003+ilyapstu@users.noreply.github.com>
2023-10-25 10:48:13 +0500

    Сделал задание 3

--- labs/lab2.py
+++ labs/lab2.py
@@ -22,3 +22,25 @@ while True:
         print("Число является пятизначным палиндромом")
     else:
         print("Число не является пятизначным палиндромом")
+#Задание 3
+number = float(input("Введите десятичную дробь: "))
+base = int(input("Введите основание новой системы счисления: "))
+integer_part = int(number)
+res: str = ""
+decimal_part = number - integer_part
+
+while integer_part > 0:
+    remainder = integer_part % base
+    res = str(remainder) + res
+    integer_part = integer_part // base
+
+if decimal_part > 0:
+    res += "."
+
+while decimal_part > 0:
+    decimal_part *= base
+    current_digit = int(decimal_part)
+    res += str(current_digit)
+    decimal_part -= current_digit
+
+print("Результат:", res)
