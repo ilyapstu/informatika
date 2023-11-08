62399ccebc5a9fc6e845adeb65938f9c7d021c01
ilyapstu <144197003+ilyapstu@users.noreply.github.com>
2023-11-08 10:44:53 +0500

    Доработать задание 6

--- labs/lab2.py
+++ labs/lab2.py
@@ -44,3 +44,19 @@ while decimal_part > 0:
     decimal_part -= current_digit
 
 print("Результат:", res)
+
+
+#Задание 6
+def months_to_buy(cost, salary):
+    months = 0
+    quarterly_salary = salary
+    while cost > 0:
+        cost *= 1.0315  # Увеличиваем цену компьютера на 3.15%
+        if months % 3 == 0 and months > 0:
+            quarterly_salary *= 1.05  # Увеличиваем зарплату каждый квартал на 5%
+            cost -= quarterly_salary
+            months += 1
+        return months
+
+
+print(months_to_buy(10000, 20000))
